434d2fafb573ad0cb1b5121465a77cc39bf511b4
liangshen001 <wangliang520wl@hotmail.com>
2023-08-04 10:41:42 +0800

    fix: 去掉无用打印

--- src/ReactCanvas.tsx
+++ src/ReactCanvas.tsx
@@ -22,165 +22,115 @@ class Image extends Layout.Image {}
 
 const reconciler = ReactReconciler<string, any, Element, Element, Text, Element, Element, any, any, any, any, any, any>({
     appendChildToContainerChildSet(childSet: any, child: any): void {
-        console.debug('appendChildToContainerChildSet')
     },
     canHydrateInstance(instance: any, type: any, props: any): any {
-        console.debug('canHydrateInstance')
     },
     canHydrateSuspenseInstance(instance: any): any {
-        console.debug('canHydrateSuspenseInstance')
     },
     canHydrateTextInstance(instance: any, text: string): any {
-        console.debug('canHydrateTextInstance')
     },
     cancelTimeout(id: any): void {
-        console.debug('cancelTimeout')
     },
     cloneHiddenInstance(instance: any, type: any, props: any, internalInstanceHandle: ReactReconciler.OpaqueHandle): any {
-        console.debug('cloneHiddenInstance')
     },
     cloneHiddenTextInstance(instance: any, text: any, internalInstanceHandle: ReactReconciler.OpaqueHandle): any {
-        console.debug('cloneHiddenTextInstance')
     },
     cloneInstance(instance: any, updatePayload: any, type: any, oldProps: any, newProps: any, internalInstanceHandle: ReactReconciler.OpaqueHandle, keepChildren: boolean, recyclableInstance: any): any {
-
-        console.debug('cloneInstance')
     },
     commitHydratedContainer(container: any): void {
-        console.debug('commitHydratedContainer')
     },
     commitHydratedSuspenseInstance(suspenseInstance: any): void {
-        console.debug('commitHydratedSuspenseInstance')
     },
     commitMount(instance: any, type: any, props: any, internalInstanceHandle: ReactReconciler.OpaqueHandle): void {
-        console.debug('commitMount')
     },
     commitTextUpdate(textInstance: Text, oldText: string, newText: string): void {
         textInstance.value = newText;
     },
     createContainerChildSet(container: any): any {
-        console.debug('createContainerChildSet')
     },
     detachDeletedInstance(node: any): void {
-        console.debug('detachDeletedInstance')
     },
     didNotFindHydratableContainerInstance(parentContainer: any, type: any, props: any): void {
-        console.debug('didNotFindHydratableContainerInstance')
     },
     didNotFindHydratableContainerSuspenseInstance(parentContainer: any): void {
-        console.debug('didNotFindHydratableContainerSuspenseInstance')
     },
     didNotFindHydratableContainerTextInstance(parentContainer: any, text: string): void {
-        console.debug('didNotFindHydratableContainerTextInstance')
     },
     didNotFindHydratableInstance(parentType: any, parentProps: any, parentInstance: any, type: any, props: any): void {
-        console.debug('didNotFindHydratableInstance')
     },
     didNotFindHydratableSuspenseInstance(parentType: any, parentProps: any, parentInstance: any): void {
-        console.debug('didNotFindHydratableSuspenseInstance')
     },
     didNotFindHydratableTextInstance(parentType: any, parentProps: any, parentInstance: any, text: string): void {
-        console.debug('didNotFindHydratableTextInstance')
     },
     didNotHydrateContainerInstance(parentContainer: any, instance: any): void {
-        console.debug('didNotHydrateContainerInstance')
     },
     didNotHydrateInstance(parentType: any, parentProps: any, parentInstance: any, instance: any): void {
-        console.debug('didNotHydrateInstance')
     },
     didNotMatchHydratedContainerTextInstance(parentContainer: any, textInstance: any, text: string): void {
-        console.debug('didNotMatchHydratedContainerTextInstance')
     },
     didNotMatchHydratedTextInstance(parentType: any, parentProps: any, parentInstance: any, textInstance: any, text: string): void {
-        console.debug('didNotMatchHydratedTextInstance')
     },
     errorHydratingContainer(parentContainer: any): void {
-        console.debug('errorHydratingContainer')
     },
     finalizeContainerChildren(container: any, newChildren: any): void {
-        console.debug('finalizeContainerChildren')
     },
     getFirstHydratableChild(parentInstance: any): any {
-        console.debug('getFirstHydratableChild')
     },
     getInstanceFromScope(scopeInstance: any): any {
-        console.debug('getInstanceFromScope')
     },
     getNextHydratableInstanceAfterSuspenseInstance(suspenseInstance: any): any {
-        console.debug('getNextHydratableInstanceAfterSuspenseInstance')
     },
     getNextHydratableSibling(instance: any): any {
-        console.debug('getNextHydratableSibling')
     },
     getParentSuspenseInstance(targetInstance: any): any {
-        console.debug('getParentSuspenseInstance')
     },
     hideInstance(instance: any): void {
-        console.debug('hideInstance')
     },
     hideTextInstance(textInstance: any): void {
-        console.debug('hideTextInstance')
     },
     hydrateInstance(instance: any, type: any, props: any, rootContainerInstance: any, hostContext: any, internalInstanceHandle: any): any[] | null {
-        console.debug('hydrateInstance')
         return null;
     },
     hydrateSuspenseInstance(suspenseInstance: any, internalInstanceHandle: any): void {
-        console.debug('hydrateSuspenseInstance')
     },
     hydrateTextInstance(textInstance: any, text: string, internalInstanceHandle: any): boolean {
-        console.debug('hydrateTextInstance')
         return false;
     },
     isPrimaryRenderer: false,
     isSuspenseInstanceFallback(instance: any): boolean {
-        console.debug('isSuspenseInstanceFallback')
         return false;
     },
     isSuspenseInstancePending(instance: any): boolean {
-        console.debug('isSuspenseInstancePending')
         return false;
     },
     noTimeout: undefined,
     preparePortalMount(containerInfo: any): void {
-        console.debug('preparePortalMount')
     },
     registerSuspenseInstanceRetry(instance: any, callback: () => void): void {
-        console.debug('registerSuspenseInstanceRetry')
     },
     replaceContainerChildren(container: any, newChildren: any): void {
-        console.debug('replaceContainerChildren')
     },
     resetTextContent(instance: any): void {
-        console.debug('resetTextContent')
     },
     scheduleTimeout(fn: (...args: unknown[]) => unknown, delay: number | undefined): any {
-        console.debug('scheduleTimeout')
     },
     supportsHydration: false,
     supportsPersistence: false,
     unhideInstance(instance: any, props: any): void {
-        console.debug('unhideInstance')
     },
     unhideTextInstance(textInstance: any, text: string): void {
-        console.debug('unhideTextInstance')
     },
     afterActiveInstanceBlur(): void {
-        console.debug('afterActiveInstanceBlur')
     },
     beforeActiveInstanceBlur(): void {
-        console.debug('beforeActiveInstanceBlur')
     },
     getCurrentEventPriority(): ReactReconciler.Lane {
-        console.debug('getCurrentEventPriority')
         return 0;
     },
     getInstanceFromNode(node: any): ReactReconciler.Fiber | null | undefined {
-        console.debug('getInstanceFromNode', node)
         return undefined;
     },
     prepareScopeUpdate(scopeInstance: any, instance: any): void {
-        console.debug('prepareScopeUpdate')
     },
     // 定义一些东西怎样与 render 环境进行交互
     supportsMutation: true,
@@ -251,7 +201,6 @@ const reconciler = ReactReconciler<string, any, Element, Element, Text, Element,
         }
     },
     insertBefore(parentInstance, child, beforeChild) {
-        console.debug('insertBefore')
         const index = parentInstance.children.findIndex(e => e === beforeChild);
         child.parent = parentInstance;
         parentInstance.children.splice(index, 0, child);
@@ -264,20 +213,9 @@ const reconciler = ReactReconciler<string, any, Element, Element, Text, Element,
     },
 
     prepareUpdate(instance, type, oldProps, newProps, rootContainerInstance, hostContext) {
-
-        if (oldProps.value !== newProps.value) {
-            return instance;
-        }
-        if (oldProps.text !== newProps.text) {
-            return instance;
-        }
-        if (oldProps.className !== newProps.className) {
-            return instance;
-        }
-        if (oldProps.style !== newProps.style) {
-            return instance;
-        }
-        if (oldProps.src !== newProps.src) {
+        if (oldProps.value !== newProps.value || oldProps.text !== newProps.text
+            || oldProps.className !== newProps.className || oldProps.style !== newProps.style
+            || oldProps.src !== newProps.src) {
             return instance;
         }
     },
@@ -298,27 +236,20 @@ const reconciler = ReactReconciler<string, any, Element, Element, Text, Element,
         }
     },
     finalizeInitialChildren(instance: any, type: any, props: any, rootContainer: any, hostContext: any): boolean {
-        console.debug('finalizeInitialChildren', instance, type, props, rootContainer, hostContext)
         return false;
     },
     getChildHostContext(parentHostContext, type, rootContainerInstance) {
-        console.debug('getChildHostContext', parentHostContext, type, rootContainerInstance)
     },
     getPublicInstance(instance) {
-        console.debug('getPublicInstance')
     },
     getRootHostContext(rootContainerInstance) {
-        console.debug('getRootHostContext', rootContainerInstance)
     },
     prepareForCommit(containerInfo: any): Record<string, any> | null {
-        console.debug('prepareForCommit')
         return null;
     },
     resetAfterCommit(containerInfo) {
-        console.debug('resetAfterCommit')
     },
     clearContainer(container) {
-        console.debug('clearContainer')
     },
     shouldSetTextContent(type, props) {
         return type === 'text';
